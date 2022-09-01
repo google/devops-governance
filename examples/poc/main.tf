4812834e6f50b629907038dc98789d6cfb148541
Alexander Meissner <94000358+amgoogle@users.noreply.github.com>
2022-09-01 11:11:11 +0200

    Update main.tf

--- examples/poc/main.tf
+++ examples/poc/main.tf
@@ -30,7 +30,7 @@ module "secret-manager-static" {
   label           = "devdps-governance-secret-static-over-stages"
   project_id      = var.project
   secret_id       = "dg-secret-static"
-  secret_version  = "pink"
+  secret_version  = "black"
   depends_on      = [module.secret-manager-variable]
 }
 
